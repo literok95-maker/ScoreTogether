64dd6f875b7e9c4e7ee4e2b26fdbdf2c729dc569
stmx <literok95@gmail.com>
2025-08-18 19:40:52 +0700

    Initial
    Signed-off-by: stmx <literok95@gmail.com>

--- composeApp/src/commonMain/kotlin/net/score/volley/demo/SportEventDetailsScreen.kt
+++ composeApp/src/commonMain/kotlin/net/score/volley/demo/SportEventDetailsScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
 
 data class SportEventDetailsScreen(val event: SportEvent) : Screen {
     @Composable
--- composeApp/src/commonMain/kotlin/net/score/volley/demo/SportsEventsScreen.kt
+++ composeApp/src/commonMain/kotlin/net/score/volley/demo/SportsEventsScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
 import net.score.volley.demo.data.InMemorySportEventRepository
 import net.score.volley.demo.domain.GetSportEventsUseCase
 import net.score.volley.demo.presentation.SportsEventsViewModel

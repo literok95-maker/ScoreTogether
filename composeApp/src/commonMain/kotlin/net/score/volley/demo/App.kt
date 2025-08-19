3e4c24e7a4d0de1383675ff4b79d6c67467c5f9a
literok95-maker <literok95@gmail.com>
2025-08-19 10:46:40 +0700

    Add profile tab

--- composeApp/src/commonMain/kotlin/net/score/volley/demo/App.kt
+++ composeApp/src/commonMain/kotlin/net/score/volley/demo/App.kt
@@ -1,14 +1,29 @@
 package net.score.volley.demo
 
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.navigator.tab.CurrentTab
+import cafe.adriel.voyager.navigator.tab.TabNavigationItem
+import cafe.adriel.voyager.navigator.tab.TabNavigator
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        Navigator(SportsEventsScreen)
+        TabNavigator(SportsEventsTab) {
+            Scaffold(
+                bottomBar = {
+                    NavigationBar {
+                        TabNavigationItem(SportsEventsTab)
+                        TabNavigationItem(ProfileTab)
+                    }
+                }
+            ) {
+                CurrentTab()
+            }
+        }
     }
 }

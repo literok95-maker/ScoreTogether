e278c81b2dbaf862561df055c1e6380b9d73b9a3
literok95-maker <literok95@gmail.com>
2025-08-20 12:42:38 +0700

    feat: add shared koin modules and platform init

--- app-web/src/wasmJsMain/kotlin/Main.kt
+++ app-web/src/wasmJsMain/kotlin/Main.kt
@@ -1,8 +1,13 @@
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.CanvasBasedWindow
 import net.scoretogether.shared.app.app
+import net.scoretogether.shared.di.initKoin
+import org.koin.dsl.module
+
+private val platformModule = module { }
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
+    initKoin(platformModule)
     CanvasBasedWindow("App") { app() }
 }

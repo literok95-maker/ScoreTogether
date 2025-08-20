348e61b5f69d4562ff43ff2bf20943f6e3655ea1
literok95-maker <literok95@gmail.com>
2025-08-20 10:39:36 +0700

    fix: add compose ui dependency for wasm

--- app-web/src/wasmJsMain/kotlin/Main.kt
+++ app-web/src/wasmJsMain/kotlin/Main.kt
@@ -1,6 +1,8 @@
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.CanvasBasedWindow
 import net.scoretogether.shared.app.app
 
+@OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     CanvasBasedWindow("App") { app() }
 }
